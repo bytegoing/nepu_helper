ce4fdc04ac0ceb68abf65c65f419fae28233fa18
BYTEGOING <i@bytegoing.com>
2020-10-23 17:42:42 +0800

    课表功能更新

--- lib/pages/class.dart
+++ lib/pages/class.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:nepu_helper/common/Global.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -13,10 +14,76 @@ class Class extends StatelessWidget {
           title: new Text("我的课表"),
           backgroundColor: Colors.blueAccent,
           centerTitle: true,
+          actions: <Widget>[
+            new PopupMenuButton<String>(
+              onSelected: (String value) {
+                switch(value) {
+                  case "changeWeek":
+                    break;
+                  case "changeXnxq":
+                    break;
+                  case "importFromJW":
+                    break;
+                  case "importFromDW":
+                    break;
+                  case "add":
+                    break;
+                }
+              },
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                PopupMenuItem(
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Text('更改周'),
+                    ],
+                  ),
+                  value: "changeWeek",
+                ),
+                PopupMenuItem(
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Text('更改学期'),
+                    ],
+                  ),
+                  value: "changeXnxq",
+                ),
+                PopupMenuItem(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new Text('导入教务系统课表'),
+                      ],
+                    ),
+                    value: "importFromJW",
+                ),
+                PopupMenuItem(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new Text('导入大物实验课表'),
+                      ],
+                    ),
+                  value: "importFromDW",
+                ),
+                PopupMenuItem(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new Text('添加课程'),
+                      ],
+                    ),
+                  value: "add",
+                )
+              ],
+            )
+          ],
         ),
-        body: new StaggeredGridView.countBuilder(
+
+        /*body: new StaggeredGridView.countBuilder(
           crossAxisCount: 8,
-        )
+        )*/
       )
     );
   }
